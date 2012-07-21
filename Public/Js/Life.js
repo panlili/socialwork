0c9e72bf6df07be73221f79fecac9b7ac446c480
libiun <libiun@gmail.com>
2012-07-21 19:50:56 +0800

    create a function init() to do some work

--- Public/Js/Life.js
+++ Public/Js/Life.js
@@ -10,27 +10,19 @@ $(function(){
     $("#idok").click(function(){
         var ss=$("input[name='idcard']").attr("value");
         idcard=ss;
-        //alert(ss);
         $("#oldinfo").empty().load("/socialwork/index.php/old/getoldinfo/"+ss,function(data){
-            //alert(data.substring(1,5));
             if(data.substring(1,5) == "老人信息"){
                 $("input[name='idcard']").focus();
             }
             else{
-              $("#service").show();  
+                $("#service").show();  
             }
         });
-        //$("#oldinfo").empty().get("/socialwork/index.php/old/getoldinfo/"+ss)
-        //         var test=$("#oldinfo").html;
-        //         alert(test);
-        
     });
     $("#service_type").change(function(){
         var str=$(this).children('option:selected').val();
-        //alert(str);
         $("#service_item").empty().load("/socialwork/index.php/old/getservicebytype/"+str,function(data){
-            //alert(data);
-        })
+            })
         $("#service_item").show();
         $("#expense").show();
     });
@@ -40,7 +32,6 @@ $(function(){
     })
     $("#expense").click(function(){
         sid=$("#service_item").children('option:selected').val();
-        //alert(sid);
         $.get("/socialwork/index.php/old/expense/"+idcard+"/"+sid,function(data){
             $("#expense_info").append(data);
         })
--- Public/Js/common.js
+++ Public/Js/common.js
@@ -27,10 +27,12 @@ function setNumber(parent) {
     }
 }
 
-//house模块, 是平方是单选按钮不同的反映
-$(function(){
+//layout.html中一些全局js效果的实现
+function init() {
+    setNumber();
     $("#common_table td").css("height","15px");
     
+    //house模块, 是平方是单选按钮不同的反映
     $(".onefloor").click(function(){
         if($(this).attr("checked")=="checked"){
             $(this).attr("value","是");
@@ -41,24 +43,22 @@ $(function(){
             $(this).parent().parent().nextUntil("#onefloor").show();
         }        
     });
-});
-
-$(function(){
-    setNumber();
     
+    //footer双击隐藏menu的实现
     $("#footer").dblclick(function(){
         $("#menu").toggle();
     });
         
+    //action_message区块
     if($.trim($("#action_message").text())==""){
         $("#action_message").remove();
     }else{
         $("#action_message").fadeOut(2500,function(){
             $(this).remove();
-        //console.log("only chrome and firebug can use console to debug js");
         });
     }
 
+    //menu的收缩
     $(".main >a").click(function(event){
         if($(this).siblings("ul").length>0){
             event.preventDefault();
@@ -66,5 +66,4 @@ $(function(){
             ulNode.slideToggle();
         }
     });
-
-});
+}
