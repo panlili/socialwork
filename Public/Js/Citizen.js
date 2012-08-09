0b2d396bfcc24909826532e9392402b934712fe9
xuefengjian <9994195@qq.com>
2012-08-09 23:37:49 +0800

    citizen.js

--- Public/Js/Citizen.js
+++ Public/Js/Citizen.js
@@ -82,7 +82,74 @@ function vertifyIdCard(idcard){
             break; 
     } 
 } 
+/**
+ * Comment
+ */
+function getAgeByBirthday(birthday) {
+    var   now=new   Date(); 
+    var   old=new   Date(birthday); 
+    var age=now.getFullYear()-old.getFullYear();
+    return age;
+    
+}
+/**
+ * 
+ */
+function getAgeByIdcard(idcard) {
+    var birthday=getBirthdayByIdcard(idcard);
+    var   now=new   Date(); 
+    var   old=new   Date(birthday); 
+    var age=now.getFullYear()-old.getFullYear();
+    return age;
 
+}
+function getSexByIdcard(idCard){
+    idCard = trim(idCard.replace(/ /g, ""));// 对身份证号码做处理。包括字符间有空格。
+    if(len(idCard)==15){
+        if(idCard.substring(14,15)%2==0){
+            return '女';
+        }else{
+            return '男';
+        }
+    }else if(len(idCard) ==18){
+        if(idCard.substring(14,17)%2==0){
+            return '女';
+        }else{
+            return '男';
+        }
+    }else{
+        return null;
+    }
+}
+/**
+ * Comment
+ */
+function getBirthdayByIddard(idcard) {
+    var birthday = "";
+    if (18 == len(idcard)) {
+        birthday = idcard.substring(6,10);
+        birthday += "-" +idcard.substring(10,12);
+        birthday += "-" + idcard.substring(12,14);
+    } else if (15 == len(idcard)) {
+        birthday = "19" + idcard.substring(6,8);
+        birthday += "-" + idcard.substring(8,10);
+        birthday += "-" + idcard.substring(10,12);
+    }
+    return birthday;
+
+}
+function len(s) { 
+    var l = 0; 
+    var a = s.split(""); 
+    for (var i=0;i<a.length;i++) { 
+        if (a[i].charCodeAt(0)<299) { 
+            l++; 
+        } else { 
+            l+=2; 
+        } 
+    } 
+    return l; 
+}
 
 function checkIdCard(idcard) {
     var message="";
@@ -90,4 +157,8 @@ function checkIdCard(idcard) {
     if("验证通过!"!=message){
         alert(message);
     }
+    
 }
+/**
+ * Comment
+ */
