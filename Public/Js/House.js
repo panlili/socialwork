995403f55ab5b7132152aa76bc245c953d1f0827
libiun <libiun@gmail.com>
2012-08-09 23:02:31 +0800

    modify house newone html

--- Public/Js/House.js
+++ Public/Js/House.js
@@ -7,5 +7,31 @@ function hideOtherAddress(element) {
     }else{
         $("#onefloor").hide();
         element.parent().parent().nextUntil("#onefloor").show();
-    }        
+    }
+}
+
+function toggleDibao(element) {
+    if(element.attr("checked")=="checked"){
+        element.attr("value","是");
+        element.parent().after("<td>低保金额：</td><td><input type=text /></td><td>开始享受低保时间：</td><td><input type=text /></td>");
+    }else{
+        element.parent().nextUntil("tr").remove();
+    }
+}
+
+function toggleLianzhu(element) {
+    if(element.attr("checked")=="checked"){
+        element.attr("value","是");
+        element.parent().after("<td>廉租地址：</td><td><input type=text /></td>");
+    }else{
+        element.parent().nextUntil("tr").remove();
+    }
+}
+
+function toggleIsFit(element){
+    if(element.attr("checked")=="checked"){
+        $(".isfit").hide();
+    }else{
+        $(".isfit").show();
+    }
 }
