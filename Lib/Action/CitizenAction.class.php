68bc613c94da1da822ee933430ac1fb77a8b852e
xuefengjian <9994195@qq.com>
2012-08-10 15:26:34 +0800

    finish the citizen add page add add method

--- Lib/Action/CitizenAction.class.php
+++ Lib/Action/CitizenAction.class.php
@@ -42,9 +42,66 @@ class CitizenAction extends BaseAction {
     }
 
     public function add() {
+        $newdata = array();
+
+        $newdata["house_id"] = $_POST["house_id"];
+        $newdata["yard_id"] = $_POST["yard_id"];
+        $newdata["name"] = $_POST["name"];
+        $newdata["relation_with_householder"] = $_POST["relation_with_householder"];
+        $newdata["id_card"] = $_POST["id_card"];
+        $newdata["nation"] = $_POST["nation"];
+        $newdata["is_fertility"] = $_POST["is_fertility"];
+        $newdata["is_special"] = $_POST["is_special"];
+        $newdata["employee"] = $_POST["employee"];
+        $newdata["household"] = $_POST["household"];
+        $newdata["telephone"] = $_POST["telephone"];
+        $newdata["education"] = $_POST["education"];
+        $newdata["political_status"] = $_POST["political_status"];
+        $newdata["is_long_live"] = $_POST["is_long_live"];
+        $newdata["marry_info"] = $_POST["marry_info"];
+
+        if ($_POST["dibao_jine"] == "") {
+            $dibao_jine = 0;
+        } else {
+            $dibao_jine = $_POST["dibao_jine"];
+        }
+        $newdata["youfu"] = array(
+            'sp_status' => $_POST["sp_status"],
+            'sp_is_xfww' => $_POST["sp_is_xfww"],
+            'sp_xfww_desc' => $_POST["sp_xfww_desc"],
+            'sp_sqjz' => $_POST["sp_sqjz"],
+            'sp_sqjz_desc' => $_POST["sp_sqjz_desc"],
+            'sp_xsjj' => $_POST["sp_xsjj"],
+            'sp_xsjj_desc' => $_POST["sp_xsjj_desc"],
+            'sp_flgcy' => $_POST["sp_flgcy"],
+            'sp_flgcy_desc' => $_POST["sp_flgcy_desc"],
+            'sp_qtry' => $_POST["sp_qtry"],
+            'sp_qtry_desc' => $_POST["sp_qtry_desc"],
+            'is_dibao' => $_POST["is_dibao"],
+            'dibao_jine' => $dibao_jine,
+            'dibao_start_date' => $_POST["dibao_start_date"],
+            'is_canji' => $_POST["is_canji"],
+            'canji_type' => $_POST["canji_type"],
+            'canji_lvl' => $_POST["canji_lvl"],
+            'canji_no' => $_POST["canji_no"],
+            'canji_jine' => $_POST["canji_jine"],
+            'is_lianzu' => $_POST["is_lianzu"],
+            'lianzu_address' => $_POST["lianzu_address"],
+        );
+
+
+
+
+
+
         $Citizen = D("Citizen");
-        $newdata = $Citizen->relation(true)->create();
-        dump($_POST);
+//        $newdata = $Citizen->relation(true)->create();
+        //$newdata[""]=$_POST[""];
+//        $newdata["youfu"]=array(
+//            'is_dibao'=>$_POST[""],
+//        );
+        dump($newdata);
+        $data = $Citizen->relation(true)->add($newdata);
 //        if ($newdata = $Citizen->create()) {
 //            dump($newdata);
 //            //$data = $Citizen->add($newdata);
@@ -121,7 +178,7 @@ class CitizenAction extends BaseAction {
             header("Content-Disposition:attachment;filename=citizen_in_house_$houseid.xls");
             $this->assign("list", $list);
             echo $this->fetch();
-        }  else {
+        } else {
             header("Content-Type:text/html; charset=utf-8");
             echo "您没有权限执行导出操作";
         }
