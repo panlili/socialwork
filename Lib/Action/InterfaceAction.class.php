767e9835e801c5abeab8df3df64f7f5bb465cf9b
libiun <libiun@gmail.com>
2012-09-07 09:41:10 +0800

    chengyue add a method to interface action

--- Lib/Action/InterfaceAction.class.php
+++ Lib/Action/InterfaceAction.class.php
@@ -211,6 +211,146 @@ class InterfaceAction extends BaseAction {
         $content = $this->fetch("_statistics");
         return $content;
     }
+    
+    //根据多个yardid, address_1 统计总数，输出列表
+	public function Getmore(){
+		if($_SESSION['right'] != '9'){
+			exit("无权限");
+		}
+		
+		if ($this->isAjax()) {
+            //解析json数据到数组
+			$yardid_array=json_decode($_POST['query_id'], true);
+			//$yardid_array=json_decode($aaa, true);
+			//exit(print_r($yardid_array));	
+            $m_yard = D("yard");
+			
+			$Alltongjiarray=array();	
+			$scope=''; $elm_sum=count($yardid_array);
+			for($i=0; $i<$elm_sum; $i++){
+				$yardid=$yardid_array[$i]['yard_id'];
+				$name=$yardid_array[$i]['name'];
+				$address_1=$yardid_array[$i]['address_1'];
+				
+				$data_yard = $m_yard->find($yardid);
+				
+				$dot=$j++!=0?'<br>':'';
+				if($i<25){ //防止输出过长信息
+					$scope.=$dot.$name;
+				}elseif($i==25){
+					$scope.=$dot.$name."<br>...省略";
+				}
+				
+				if (!empty($data_yard)) {
+					$address_2 = $this->hasCollection($yardid, $address_1);
+					$houses = $this->houseCollection($yardid, $address_1);
+					$tongjiarray = $this->statistics_getmore($houses, $yardid, $address_1, $address_2, "", $address_1 . "栋");
+					
+					//累加到一起
+					$Alltongjiarray['housenumber']+=$tongjiarray['housenumber'];
+					$Alltongjiarray['citizensum']+=$tongjiarray['citizensum'];
+					$Alltongjiarray['citizenzhanzhu']+=$tongjiarray['citizenzhanzhu'];
+					$Alltongjiarray['citizenparty']+=$tongjiarray['citizenparty'];
+					$Alltongjiarray['citizendibao']+=$tongjiarray['citizendibao'];
+					$Alltongjiarray['houseislowrent']+=$tongjiarray['houseislowrent'];
+					$Alltongjiarray['houseisfuel']+=$tongjiarray['houseisfuel'];
+					$Alltongjiarray['citizenislonglive']+=$tongjiarray['citizenislonglive'];
+					$Alltongjiarray['citizendisable']+=$tongjiarray['citizendisable'];
+					$Alltongjiarray['citizenspecial']+=$tongjiarray['citizenspecial'];
+					$Alltongjiarray['housetaiwan']+=$tongjiarray['housetaiwan'];
+					$Alltongjiarray['housearmy']+=$tongjiarray['housearmy'];
+					
+				} else {
+					echo "<br>无此id的院落";
+				}
+			
+			}
+
+			$this->assign(array("tongji" => $Alltongjiarray, "scope" => $scope, "elm_sum" => $elm_sum));
+			$content = $this->fetch("_getmore");
+			echo $content;			
+			return $content;
+        } else {
+           echo "请用ajax调用方法";
+        }
+	}
+	
+	//为Getmore返回统计数组
+    protected function statistics_getmore($houses, $yardid, $address_1 = "", $address_2 = "", $address_3 = "", $scope = "") {
+        $tongjiarray = array();
+
+        //获取此院落下居民人数, 暂住人口, 现有住户, 党员
+        $citizensum = 0;
+        $citizenzhanzhu = 0;
+        $housenumber = count($houses);
+        $citizenparty = 0;
+        //低保, 享受廉租房(house中的is_lowrent), 享受燃煤补贴(house中的is_fuel), 享受长寿金
+        $citizendibao = 0;
+        $houseislowrent = 0;
+        $houseisfuel = 0;
+        $citizenislonglive = 0;
+        //残疾人,特殊人员,台属,军属
+        $citizendisable = 0;
+        $citizenspecial = 0;
+        $housetaiwan = 0;
+        $housearmy = 0;
+
+        $citizen = D("Citizen");
+        foreach ($houses as $house) {
+            $house_id = $house["id"];
+            $citizensum+=$citizen->where(array("house_id" => $house_id))->count();
+            $citizenzhanzhu+=$citizen->where(array("house_id" => $house_id, "relation_with_householder" => "暂住人口"))->count();
+            $citizenparty+=$citizen->where(array("house_id" => $house_id, "political_status" => "党员"))->count();
+            $citizendibao+=$citizen->where(array("house_id" => $house_id, "is_low_level" => "是"))->count();
+            $citizenislonglive+=$citizen->where(array("house_id" => $house_id, "is_long_live" => "是"))->count();
+            $citizendisable+=$citizen->where(array("house_id" => $house_id, "is_disability" => "是"))->count();
+            $citizenspecial+=$citizen->where(array("house_id" => $house_id, "is_special" => "是"))->count();
+
+            $houseislowrent+=$house["is_lowrent"] == "是" ? 1 : 0;
+            $houseisfuel+=$house["is_fuel"] == "是" ? 1 : 0;
+            $housetaiwan+=$house["is_taiwan"] == "是" ? 1 : 0;
+            $housearmy+=$house["is_army"] == "是" ? 1 : 0;
+        }
+
+        //各个层次房屋总数的链接
+        $tongjiarray["housenumber"] = $housenumber;
+
+        //各层次享受廉租福利
+        $tongjiarray["houseislowrent"] = $houseislowrent;
+
+        //各层次享受燃油补贴
+        $tongjiarray["houseisfuel"] = $houseisfuel;
+
+        //各层次台属
+        $tongjiarray["housetaiwan"] = $housetaiwan;
+
+        //各层次军属
+        $tongjiarray["housearmy"] = $housearmy;
+
+        //各层次居民
+        $tongjiarray["citizensum"] = $citizensum;
+
+        //居民是否是党员那些，因为可以导出excel，一下就能筛选，就不做了。
+        $tongjiarray["citizenzhanzhu"] = $citizenzhanzhu;
+        $tongjiarray["citizenparty"] = $citizenparty;
+        $tongjiarray["citizendibao"] = $citizendibao;
+        $tongjiarray["citizenislonglive"] = $citizenislonglive;
+        $tongjiarray["citizendisable"] = $citizendisable;
+        $tongjiarray["citizenspecial"] = $citizenspecial;
+
+        //栋数的buttons
+        if ("" == $address_3 && "" == $address_2 && "" != $address_1 && "" != $yardid)
+            $this->assign(array("yardid" => $yardid, "address_1" => $address_1));
+        //单元的buttons
+        if ("" == $address_3 && "" != $address_2 && "" != $address_1 && "" != $yardid)
+            $this->assign(array("yardid" => $yardid, "address_1" => $address_1, "address_2" => $address_2));
+        //楼层的buttons
+        if ("" != $address_3 && "" != $address_2 && "" != $address_1 && "" != $yardid)
+            $this->assign(array("yardid" => $yardid, "address_1" => $address_1, "address_2" => $address_2, "address_3" => $address_3));
+
+        
+        return $tongjiarray;
+    }
 
 }
 
